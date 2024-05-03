56401c3a0d54850b0ee32d77f7aeddf6aefefb00
unknown <jugada160324@gmail.com>
2024-05-03 16:20:54 -0500

    Feat: Se consigue implementar el toast del manejo de error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document
   .getElementById("registerForm")
-  .addEventListener("submit", function (event) {
+  ?.addEventListener("submit", function (event) {
     event.preventDefault();
 
     var formData = new FormData(this);
@@ -26,3 +26,44 @@ document
         console.error("Error:", error);
       });
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("loginForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      var formData = new FormData(this);
+
+      var formObject = {};
+      formData.forEach(function (value, key) {
+        formObject[key] = value;
+      });
+
+      fetch("http://localhost:8080/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formObject),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.error == "Correo electrónico o contraseña incorrectos") {
+            console.log("Error: " + data.error);
+
+            document.getElementById("errorDiv").style.display = "block";
+
+            // Después de 2 segundos, oculta el div de error
+            setTimeout(() => {
+              document.getElementById("errorDiv").style.display = "none";
+            }, 2000);
+          } else {
+            window.location.href = "dashboard.html";
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+});
