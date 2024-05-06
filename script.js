364ae1e1238f350d265e867c3c84275f3b0c19f3
unknown <jugada160324@gmail.com>
2024-05-06 17:34:01 -0500

    Feat: Se consigue avance par manejo de sesion

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Manejo del formulario de inicio de sesión
 document
-  .getElementById("registerForm")
+  .getElementById("loginForm")
   ?.addEventListener("submit", function (event) {
     event.preventDefault();
 
@@ -10,123 +11,104 @@ document
       formObject[key] = value;
     });
 
-    fetch("http://localhost:8080/api/register", {
+    fetch("http://localhost:8080/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formObject),
+      credentials: "include", // Enviar cookies
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        return response.json();
+      })
       .then((data) => {
-        console.log(data);
-        window.location.href = "index.html";
+        localStorage.setItem("user_id", data.userId);
+
+        if (
+          data &&
+          data.error == "Correo electrónico o contraseña incorrectos"
+        ) {
+          console.log("Error: " + data.error);
+          // Aquí podrías mostrar un mensaje de error al usuario
+        } else {
+          // Aquí podrías guardar la información del usuario en el cliente
+          console.log("Inicio de sesión exitoso");
+          location.href = "dashboard.html";
+        }
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   });
 
-document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("loginForm")
-    ?.addEventListener("submit", function (event) {
-      event.preventDefault();
-
-      var formData = new FormData(this);
-
-      var formObject = {};
-      formData.forEach(function (value, key) {
-        formObject[key] = value;
-      });
-
-      fetch("http://localhost:8080/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formObject),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.error == "Correo electrónico o contraseña incorrectos") {
-            console.log("Error: " + data.error);
-
-            document.getElementById("errorDiv").style.display = "block";
-
-            // Después de 2 segundos, oculta el div de error
-            setTimeout(() => {
-              document.getElementById("errorDiv").style.display = "none";
-            }, 2000);
-          } else {
-            window.location.href = "dashboard.html";
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    });
-});
-
+// Manejo del botón de cerrar sesión
 document.getElementById("btnLogOut")?.addEventListener("click", function () {
   console.log("El botón Cerrar sesión ha sido presionado.");
 
   fetch("http://localhost:8080/api/logOut", {
     method: "GET",
+    credentials: "include", // Enviar cookies
   })
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      window.location.href = "index.html";
+      // Aquí podrías limpiar la información de sesión en el cliente
+      window.location.href = "index.html"; // Redirigir a la página principal
     })
     .catch((error) => console.error("Error:", error));
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  document
-    .getElementById("uploadForm")
-    ?.addEventListener("submit", function (event) {
-      event.preventDefault();
+// Manejo del formulario de carga de archivos
+document
+  .getElementById("uploadForm")
+  ?.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-      var formData = new FormData(this);
+    var formData = new FormData(this);
 
-      fetch("http://localhost:8080/api/magazine", {
-        method: "POST",
-        body: formData,
+    fetch("http://localhost:8080/api/magazine", {
+      method: "POST",
+      body: formData,
+      credentials: "include", // Enviar cookies
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error en la solicitud");
+        }
+        return response.json();
       })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Error en la solicitud");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          if (data.pdfURL) {
-            // Abre el PDF en una nueva ventana del navegador
-            window.open(`http://localhost:8080${data.pdfURL}`, "_blank");
-          } else {
-            console.error("No se ha proporcionado la URL del PDF");
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
+      .then((data) => {
+        console.log(data);
+        if (data.pdfURL) {
+          // Abre el PDF en una nueva ventana del navegador
+        } else {
+          console.error("No se ha proporcionado la URL del PDF");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
 
-      fetch("http://localhost:8080/api/user")
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("No has iniciado sesión");
-          }
-        })
-        .then((user) => {
-          console.log(user);
-          // Aquí puedes hacer lo que quieras con la información del usuario
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    });
-});
+    fetch("http://localhost:8080/api/user", {
+      credentials: "include", // Enviar cookies
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("No has iniciado sesión");
+        }
+      })
+      .then((user) => {
+        console.log(user);
+        // Aquí puedes hacer lo que quieras con la información del usuario
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
